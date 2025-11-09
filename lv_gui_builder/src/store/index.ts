f396b0ea99b0d877cab0687d42a72901f6b2e7d6
Collector985 <kelly.brezovski@gmail.com>
2025-11-09 07:39:52 -0800

    Remove lv_gui_builder

--- lv_gui_builder/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { createPinia } from 'pinia';
-
-const pinia = createPinia();
-
-export default pinia;
-
-export * from './modules/user';
-export * from './modules/menu';
--- lv_gui_builder/src/utils/http/types.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-// 和后端约定好接口返回的数据结构
-export interface Response<T = any> {
-  code: number | string;
-  message: string;
-  result: T;
-}
